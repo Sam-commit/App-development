da3eed6a48318cf89e79a626fb66c6e46adea288
Sam-commit <iit2020264@iiita.ac.in>
2022-02-09 03:18:36 +0530

    To-do app

---
+++ companion/lib/homepage.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40, bottom: 10, left: 30),
+            child: CircleAvatar(
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.menu_rounded,
+                    size: 30,
+                    color: Colors.blue.withOpacity(0.8),
+                  )),
+              backgroundColor: Colors.white,
+              radius: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 10),
+            child: Text(
+              "Your Task ",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Text(
+              "Maestro",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, bottom: 30),
+            child: Text(
+              "12 Tasks remaining ",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                  color: Colors.white),
+              child: ListView(),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
